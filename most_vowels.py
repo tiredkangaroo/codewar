7269d75ea682754094695d91889551a26af5cb63
KokoTheBest <81335306+KokoTheBest@users.noreply.github.com>
2022-01-01 16:06:46 -0500

    add challenges

---
+++ most_vowels.py
@@ -0,0 +1,11 @@
+def most_vowels(sentence):
+    currentWord = ["", 0]
+    for s in sentence.split():
+        vowels = 0
+        for i in s:
+            if i in ["a", "e", "i", "o", "u"]:
+                vowels += 1
+        if vowels > currentWord[1]:
+            currentWord = [s, vowels]
+    return currentWord[0]
+print(most_vowels("what a wonderful life")) #=> "wonderful"
