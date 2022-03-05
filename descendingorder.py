f18832a894d487d717d2a7915861478c108ff23e
Ajitesh Kumar <ajinest6@gmail.com>
2022-03-05 16:59:49 -0500

    help comes from the future

--- descendingorder.py
+++ descendingorder.py
@@ -4,3 +4,11 @@ def descending_order(num):
     numl.reverse()
     return int("".join(numl))
 print(descending_order(32456452145))
+
+# Pulled from another solution, we could've done:
+# numl.sort(reverse=True) which auto reverses it, making it 3, 2, 1 instead of 1, 2, 3
+# We could also make it a one-liner:
+# return int("".join([m for m in str(num)].sort(reverse=True)))
+# Pulled from another solution, we could've used the sorted function for a string instead of using .sort for an array
+# return int("".join(sorted(str(num), reverse=True))
+# Remember, sorted return an array of the sorted string, not a string, so we can't just do int(sorted(str(num), reverse=True))
